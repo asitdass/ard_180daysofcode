32a14bb52ecba721e3e583e80869a908c87db3bb
Asit Rohan Dass <57055405+asitdass@users.noreply.github.com>
2023-10-08 22:56:36 +0530

    Create trailingZeroesinFact.cpp
    https://practice.geeksforgeeks.org/problems/trailing-zeroes-in-factorial5134/1

---
+++ trailingZeroesinFact.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int trailingZeroes(int N)
+    {
+        int count = 0;
+        while(N>=5){
+            count += N/5;
+            N = N/5;
+        }
+        return count;
+    }
+};
